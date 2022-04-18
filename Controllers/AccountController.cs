13e0de7e0f3b8f01a357d57b5830943ca34a1851
rvanfrachen-wctc <rvanfrachen@my.wctc.edu>
2022-04-18 18:48:32 -0500

    Added sending email and page the gets email

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -3,6 +3,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Northwind.Models;
+using System;
+using System.Net;
+using System.Net.Mail;
 
 namespace Northwind.Controllers
 {
@@ -47,6 +50,68 @@ namespace Northwind.Controllers
         {
             await _signInManager.SignOutAsync();
             return RedirectToAction("Index", "Home");
+        }
+
+        // public ActionResult ForgotPassword()
+        // {
+        //     return View();
+        // }
+        // [HttpPost]
+        // public ActionResult ForgotPassword(ForgotPasswordViewModel model)
+        // {
+        //     string to = "ryanj34521@gmail.com"; //To address    
+        //     string from = "northwindfp.help@gmail.com"; //From address    
+        //     MailMessage message = new MailMessage(from, to);
+
+        //     string mailbody = "Click this link to reset your password: ";
+        //     message.Subject = "Reset Password";
+        //     message.Body = mailbody;
+        //     // message.BodyEncoding = Encoding.UTF8;  
+        //     message.IsBodyHtml = true;
+        //     SmtpClient client = new SmtpClient("smtp.gmail.com", 587); //Gmail smtp    
+        //     System.Net.NetworkCredential basicCredential1 = new
+        //     System.Net.NetworkCredential("northwindfp.help@gmail.com", "Qc#rXTVF6@2WNpf");
+        //     client.EnableSsl = true;
+        //     client.UseDefaultCredentials = false;
+        //     client.Credentials = basicCredential1;
+        //     try
+        //     {
+        //         client.Send(message);
+        //     }
+        //     catch (Exception Ex)
+        //     {
+        //         throw Ex;
+        //     }
+
+        //     return View();
+        // }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> ForgotPassword(ForgotPasswordModel model)
+        {
+            if (ModelState.IsValid)
+            {
+            //Send Email
+            MailMessage Msg = new MailMessage();
+            Msg.From = new MailAddress("northwindfp.help@gmail.com","Northwind");// replace with valid value
+            Msg.Subject = "Reset Password";
+            Msg.To.Add(model.Email); //replace with correct values
+            Msg.Body = "Click this link to reset your password: ";
+            Msg.IsBodyHtml = true;
+            Msg.Priority = MailPriority.High;
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = "smtp.gmail.com";
+            smtp.Port = 587;
+            smtp.Credentials = new System.Net.NetworkCredential("northwindfp.help@gmail.com", "Qc#rXTVF6@2WNpf");// replace with valid value
+            smtp.EnableSsl = true;
+            smtp.Timeout = 20000;
+
+            smtp.Send(Msg);
+            return RedirectToAction("Login");
+            }
+            
+            return View(model);
         }
 
         public ViewResult AccessDenied() => View();
